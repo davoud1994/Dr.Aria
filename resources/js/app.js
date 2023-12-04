21e0cf8628c81857f3a0f78d0062001f216ff6fd
davoud1994 <davoudgh1373@gmail.com>
2023-12-04 08:13:13 +0330

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,38 @@
+import './bootstrap';
+import "./jquery.min";
+
+import "./jquery-migrate-3.0.0";
+
+import "./jquery-ui.min";
+
+import "./easing";
+
+import "./colors";
+
+// import "./popper.min";
+
+import "./bootstrap-datepicker";
+
+import "./jquery.nav";
+import "./slicknav.min";
+
+import "./jquery.scrollUp.min";
+
+import "./niceselect";
+
+import "./tilt.jquery.min";
+
+import"./owl-carousel";
+
+import "./jquery.counterup.min";
+
+// import "./steller";
+
+// import "./wow.min";
+
+
+
+
+import "./bootstrap.min.js";
+
+import "./main.js";
