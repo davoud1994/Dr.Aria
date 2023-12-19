8439336fadec1a62a33feb2e6821e04b62314b89
MoloodNaseralmemarin <mnaseri208@yahoo.com>
2023-12-19 13:10:57 +0330

    3 commit

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -986,7 +986,7 @@
 															پروژه‌های موفقی را در حوزه‌های مختلف پزشکی به انجام رسانده و محصولات حرفه‌ای در زمینه حوزه
 															سلامت تولید نماید و مورد تایید پارک علم و فناوری، تحت نظر وزارت علوم، تحقیقات و فناوری قرار
 															گیرد.
-													
+
                             </p>
 
                             <!-- Social -->
@@ -995,32 +995,33 @@
                                 <li><a href="#"><i class="icofont-email"></i></a></li>
                                 <li><a href="#"><i class="fa fa-mobile-phone"></i></a></li>
                                 <li><a href="#"><i class="fa fa-phone"></i></a></li>
-                                
+                                <li><a href="#"><i class="fa fa-address-book"></i></a></li>
+
                             </ul>
                             <!-- End Social -->
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 col-12">
                         <div class="single-footer f-link">
-                            <h2>Quick Links</h2>
+                            <h2>لینک های مهم</h2>
                             <div class="row">
-                                <div class="col-lg-6 col-md-6 col-12">
-                                    <ul>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Home</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>About Us</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Services</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Our
-                                                Cases</a></li>
-                                        <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Other Links</a></li>
+                                <div class="col-lg-20 col-md-20">
+                                    <ul class='import-links-footer'>
+                                        <li><a href="#"><i class="fa fa-caret-left"aria-hidden="true"></i>
+                                                سوالات مهم</a></li>
+                                        <li><a href="#"><i class="fa fa-caret-left"
+                                                    aria-hidden="true"></i>مقالات</a></li>
+                                        <li><a href="#"><i class="fa fa-caret-left" aria-hidden="true"> </i>
+                                                   خدمات</a></li>
+                                        {{-- <li><a href="#"><i class="fa fa-caret-right" aria-hidden="true"></i>Our
+                                                Cases</a></li> --}}
+                                        <li><a href="#"><i class="fa fa-caret-left"
+                                                    aria-hidden="true"></i>خانه </a></li>
                                     </ul>
                                 </div>
                                 <div class="col-lg-6 col-md-6 col-12">
                                     <ul>
-                                        <li><a href="#"><i class="fa fa-caret-right"
+                                        {{-- <li><a href="#"><i class="fa fa-caret-right"
                                                     aria-hidden="true"></i>Consuling</a></li>
                                         <li><a href="#"><i class="fa fa-caret-right"
                                                     aria-hidden="true"></i>Finance</a></li>
@@ -1029,7 +1030,7 @@
                                         <li><a href="#"><i class="fa fa-caret-right"
                                                     aria-hidden="true"></i>FAQ</a></li>
                                         <li><a href="#"><i class="fa fa-caret-right"
-                                                    aria-hidden="true"></i>Contact Us</a></li>
+                                                    aria-hidden="true"></i>Contact Us</a></li> --}}
                                     </ul>
                                 </div>
                             </div>
@@ -1037,26 +1038,26 @@
                     </div>
                     <div class="col-lg-3 col-md-6 col-12">
                         <div class="single-footer">
-                            <h2>Open Hours</h2>
-                            <p>Lorem ipsum dolor sit ame consectetur adipisicing elit do eiusmod tempor incididunt.</p>
-                            <ul class="time-sidual">
+                            <h2>آدرس شرکت</h2>
+                            <p><p>همدان - چهاراه تختی- &nbsp;خیابان شهید غفاری - مجتمع نرم افزار پزشکی آریا دکتر</p></p>
+                            {{-- <ul class="time-sidual">
                                 <li class="day">Monday - Fridayp <span>8.00-20.00</span></li>
                                 <li class="day">Saturday <span>9.00-18.30</span></li>
                                 <li class="day">Monday - Thusday <span>9.00-15.00</span></li>
-                            </ul>
+                            </ul> --}}
                         </div>
                     </div>
-                    <div class="col-lg-3 col-md-6 col-12">
+                    <div class="col-lg-6 col-md-6 col-12">
                         <div class="single-footer">
-                            <h2>Newsletter</h2>
+                            {{-- <h2>Newsletter</h2>
                             <p>subscribe to our newsletter to get allour news in your inbox.. Lorem ipsum dolor sit
                                 amet, consectetur adipisicing elit,</p>
                             <form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
                                 <input name="email" placeholder="Email Address" class="common-input"
                                     onfocus="this.placeholder = ''" onblur="this.placeholder = 'Your email address'"
                                     required="" type="email">
-                                <button class="button"><i class="icofont icofont-paper-plane"></i></button>
-                            </form>
+                                <button class="button"><i class="icofont icofont-paper-plane"></i></button> --}}
+                            {{-- </form> --}}
                         </div>
                     </div>
                 </div>
